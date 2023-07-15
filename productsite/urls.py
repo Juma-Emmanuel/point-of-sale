0650adccf2abf5ed169aed1544983442e1143f82
Emmanuel juma <jumaemmanuel462@yahoo.com>
2023-07-15 08:23:49 +0300

    update

--- productsite/urls.py
+++ productsite/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
    path('',views.home, name="home"), 
-   path('postsign/',views.postsign, name="postsign"), 
+   path('postsign/',views.postsign, name="postsign"),
+   path('logout/',views.logout, name="log"), 
+   path('signUp/',views.signUp, name="signup"), 
+   path('postsignup/',views.postsignup, name="postsignup"),
 ]
--- productsite/views.py
+++ productsite/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 import pyrebase
+from django.contrib import auth
 # Create your views here.
 
 config = {
@@ -13,16 +14,50 @@ config = {
   }
 firebase = pyrebase.initialize_app(config)
 
-auth = firebase.auth()
+authe = firebase.auth()
+database=firebase.database()
 
 def home(request):
 	return render(request,"signIn.html",{})
+def signUp(request):
+    return render(request, "signUp.html",)
 
-def postsign(request):
-	email=request.POST.get('email')
-	passw = request.POST.get('pass')
-	user= auth.sign_in_with_email_and_password(email,passw)
+def postsignup(request):
     
+    name=request.POST.get('name')
+    email = request.POST.get('email')
+    passw = request.POST.get('pass')
+    try:
+        user=authe.create_user_with_email_and_password(email,passw)
+    except:
+        message = "unable to create try again"
+        return render(request, "signUp.html",{"messg":message})
+        uid = user['localId']
+
+    data={"name":name,"status":"1"}
+
+    database.child("users").child(uid).child("details").set(data)
+    return render(request,"signIn.html",)
+def postsign(request):
+    name=request.POST.get('name')
+    email = request.POST.get('email')
+    passw = request.POST.get('pass')
+    try:
+        user = authe.sign_in_with_email_and_password(email, passw,)
+    except:
+        message = "invalid credentials"
+        return render(request, "signIn.html",{"messg":message})
+    print(user['idToken'])
+    session_id=user['idToken']
+    request.session['uid']=str(session_id)
+    return render(request, "welcome.html", {"n":name})
+
+def logout(request):
+    auth.logout(request)
+    return render(request,"signIn.html",)
 
 
-	return render(request,"welcome.html",{})
+
+
+
+
