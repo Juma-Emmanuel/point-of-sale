6833720f2192ad030aa30c32637500d9de745c4f
Emmanuel juma <jumaemmanuel462@yahoo.com>
2023-07-17 05:35:26 +0300

    update

--- productsite/views.py
+++ productsite/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 import pyrebase
 from django.contrib import auth
-# Create your views here.
+import firebase_admin
 
+
+
+
+
+
+
+# Create your views here.
+# Initialize the Firebase Admin SDK
+#cred = credentials.Certificate('E:/myprograms/productapp/serviceAccountKey.json')
+#second_app = firebase_admin.initialize_app(cred, name="second_app")
 config = {
     "apiKey": "AIzaSyD0BtuWBuMH4lDxhJKMaQghop2ebpkc7-k",
     "authDomain": "cpanel-c8e63.firebaseapp.com",
@@ -17,6 +27,7 @@ firebase = pyrebase.initialize_app(config)
 authe = firebase.auth()
 database=firebase.database()
 
+ 
 def home(request):
 	return render(request,"signIn.html",{})
 def signUp(request):
@@ -29,15 +40,27 @@ def postsignup(request):
     passw = request.POST.get('pass')
     try:
         user=authe.create_user_with_email_and_password(email,passw)
+        uid=user['localId']
     except:
         message = "unable to create try again"
         return render(request, "signUp.html",{"messg":message})
-        uid = user['localId']
+        
 
     data={"name":name,"status":"1"}
 
     database.child("users").child(uid).child("details").set(data)
     return render(request,"signIn.html",)
+
+'''def get_user_name(request):
+
+    #if request.user.is_authenticated:
+        # Retrieve the name from the currently authenticated user
+    user_name = request.user.name  # Replace 'name' with the actual attribute name
+
+    return user_name
+    #else:
+   '''  #   return None
+
 def postsign(request):
     name=request.POST.get('name')
     email = request.POST.get('email')
@@ -47,10 +70,18 @@ def postsign(request):
     except:
         message = "invalid credentials"
         return render(request, "signIn.html",{"messg":message})
-    print(user['idToken'])
-    session_id=user['idToken']
-    request.session['uid']=str(session_id)
-    return render(request, "welcome.html", {"n":name})
+        print(user['localId'])
+    
+    context = {  
+        
+        "user_role": 'user_rolle'
+    }
+    check=5
+    if email =='user6@gmail.com':
+        return render(request, "welcome.html", )
+    else:
+        
+        return render(request, "welcome.html",context)
 
 def logout(request):
     auth.logout(request)
